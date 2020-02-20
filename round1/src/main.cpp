aea447c7fe1995a6c374c870dfebfcf548ed8ce9
Andras Kucsma <andras.kucsma@prezi.com>
2020-02-20 19:32:29 +0100

    Move print to parse.hpp

--- round1/src/main.cpp
+++ round1/src/main.cpp
@@ -5,24 +5,6 @@
 
 
 
-void print(std::ostream& stream, const OutData& data) {
-	stream << data.libraries.size() << std::endl;
-	for (auto& item : data.libraries) {
-		stream << item.library_idx << " " << item.books.size() << std::endl;
-		bool first = true;
-		for (auto& k : item.books) {
-			if (first) {
-				first = false;
-			} else {
-				stream << " ";
-			}
-			stream << k;
-		}
-		stream << std::endl;
-	}
-}
-
-
 int main() {
 
 
--- round1/src/parse.hpp
+++ round1/src/parse.hpp
@@ -64,6 +64,22 @@ inline InData parse(std::istream& stream) {
 	return data;
 }
 
+inline void print(std::ostream& stream, const OutData& data) {
+	stream << data.libraries.size() << std::endl;
+	for (auto& item : data.libraries) {
+		stream << item.library_idx << " " << item.books.size() << std::endl;
+		bool first = true;
+		for (auto& k : item.books) {
+			if (first) {
+				first = false;
+			} else {
+				stream << " ";
+			}
+			stream << k;
+		}
+		stream << std::endl;
+	}
+}
 
 
 #endif // PARSE_HPP_
