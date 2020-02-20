4e8d5877f3c6cb63fd000681e116fa3b669eede5
Andras Kucsma <andras.kucsma@prezi.com>
2020-02-20 20:48:35 +0100

    Fix scoring

--- round1/check.sh
+++ round1/check.sh
@@ -7,10 +7,13 @@ BINARY=""
 [ -f "build/osx-x64-ninja-debug/solve" ] && BINARY="build/osx-x64-ninja-debug/solve"
 [ -f "build/osx-x64-ninja-debug/solve" ] && BINARY="build/osx-x64-ninja-debug/greedy"
 
+[ -f "build/score" ] && SCORE="build/score"
+[ -f "build/osx-x64-ninja-debug/score" ] && SCORE="build/osx-x64-ninja-debug/score"
+
 [ -f "${BINARY}" ] || echo "No binary found"
 [ -f "${BINARY}" ] || exit 1
 
-echo "Using binary: ${BINARY}"
+echo "Using binary: ${BINARY} and ${SCORE}"
 
 mkdir -p output
 
@@ -19,5 +22,5 @@ for f in input/*.txt; do
 
     "${BINARY}" < "${f}" > "output/$(basename ${f})"
     echo -n "  Score: "
-    cat "output/$(basename ${f})" | build/score "${f}" | tail -n 1 | cut -d ' ' -f 2
+    cat "output/$(basename ${f})" | "${SCORE}" "${f}" | tail -n 1 | cut -d ' ' -f 2
 done
