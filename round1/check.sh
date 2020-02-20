630aead235c90473dfda5b1a73ad1fe86c86e09d
Matyas Vegh <matyas.vegh@ericsson.com>
2020-02-20 20:45:36 +0100

    Print score during tests

--- round1/check.sh
+++ round1/check.sh
@@ -3,6 +3,7 @@
 BINARY=""
 
 [ -f "build/solve" ] && BINARY="build/solve"
+[ -f "build/greedy" ] && BINARY="build/greedy"
 [ -f "build/osx-x64-ninja-debug/solve" ] && BINARY="build/osx-x64-ninja-debug/solve"
 [ -f "build/osx-x64-ninja-debug/solve" ] && BINARY="build/osx-x64-ninja-debug/greedy"
 
@@ -17,4 +18,6 @@ for f in input/*.txt; do
     echo "----- Checking ${f} -----"
 
     "${BINARY}" < "${f}" > "output/$(basename ${f})"
+    echo -n "  Score: "
+    cat "output/$(basename ${f})" | build/score "${f}" | tail -n 1 | cut -d ' ' -f 2
 done
